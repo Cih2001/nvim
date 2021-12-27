20b5f9cb5144098fe0b9d1d141cbc952d8873cdf
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2021-12-27 01:36:17 +0100

    initial commit

---
+++ lua/user/indentline.lua
@@ -0,0 +1,20 @@
+local status_ok, indent_blankline = pcall(require, "indent_blankline")
+if not status_ok then
+  return
+end
+
+vim.g.indentLine_enabled = 1
+-- vim.g.indent_blankline_char = "│"
+vim.g.indent_blankline_char = "▏"
+indent_blankline.setup({
+	-- show_end_of_line = true,
+	-- space_char_blankline = " ",
+	-- show_current_context = true,
+  -- show_char_blankline = " "
+	-- show_current_context_start = true,
+	-- char_highlight_list = {
+	--   "IndentBlanklineIndent1",
+	--   "IndentBlanklineIndent2",
+	--   "IndentBlanklineIndent3",
+	-- },
+})
