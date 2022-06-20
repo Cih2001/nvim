d5e6d681d4372a5ff59f0590aad762aeb851f0da
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2022-06-20 02:53:24 +0200

    finally done with place holders 

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -1,10 +1,8 @@
-if not pcall(require, "luasnip") then
+local status, ls = pcall(require, "luasnip")
+if not status then
   return
 end
 
-print("hello there")
-
-local ls = require "luasnip"
 local types = require "luasnip.util.types"
 
 ls.config.set_config {
@@ -208,7 +206,7 @@ ls.add_snippets(nil, {
 
 -- <c-k> is my expansion key
 -- this will expand the current item or jump to the next item within the snippet.
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
@@ -216,7 +214,7 @@ end, { silent = true })
 
 -- <c-j> is my jump backwards key.
 -- this always moves to the previous item within the snippet
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
@@ -231,6 +229,3 @@ vim.keymap.set("i", "<c-l>", function()
 end)
 
 vim.keymap.set("i", "<c-u>", require "luasnip.extras.select_choice")
-
--- shorcut to source my luasnips file again, which will reload my snippets
-vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
