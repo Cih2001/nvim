49305b746643af56da5b8529a0c9b7b1d631582e
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2023-01-12 20:42:24 +0100

    textcortex initial

--- lua/user/textcortex.lua
+++ lua/user/textcortex.lua
@@ -6,47 +6,60 @@ local action_state = require("telescope.actions.state")
 local previewers = require("telescope.previewers")
 
 -- This test paragraph contains some intentional errors.
+-- There are many benefits to exercise for both the body and the mind. Regular exercise can improve
+-- cardiovascular health, increased strength and flexibility, and reduce
+-- the risk of chronic diseases such as obesity, type 2 diabetes, and heart disease.
+-- Exercise has been shown to improve mood, both physically and emotionally.
+-- Regular physical activity can help improve sleep quality and duration.
+-- There are countless ways to get active each day, whether you prefer to participate in organized
+-- sports, go for a jog, or simply take a walk around the block.
+-- Making an activity you enjoyed into a regular part of your life is essential.
+-- Physical exercise can help you feel better both mentally and physically.
+-- Start incorporating it into your routine today!
 --
--- Exercise has numerous benefits for the body and the mind. It can improved cardiovascular health,
--- increased strength and flexibility, and reduced the risk of chronic diseases such as
--- obesity, type 2 diabetes, and heart disease. In addition to physical benefits,
--- exercise have been shown to improved mood.
--- function and mental clarity. Regular physical activity can also helped to improved sleep quality
--- and duration. Whether you preferred to participate in organized sports, go for a jog,
--- or simply take a walk around the block, there are countless ways to incorporate exercise into
--- your daily routine. The key is to find an activity that you enjoyed and to make it
--- a consistent part of your lifestyle. So if you want to feel better, both
--- physically and mentally, start incorporating exercise into your routine today!
---
-
--- This test paragraph contains some intentional errors. This
--- is the second item. And this is the third.
 
 local M = {
 	callback = function(selection)
 		vim.fn.execute("normal! vis")
-		local right = vim.fn.getpos("'>")
-		local right_row = right[2] - 1
-		local right_col = right[3] - 1
-
 		local left = vim.fn.getpos("'<")
 		local left_row = left[2] - 1
 		local left_col = left[3] - 1
-		vim.pretty_print(vim.api.nvim_buf_get_text(0, left_row, left_col, right_row, right_col, {}))
+		local right = vim.fn.getpos("'>")
+		local right_row = right[2] - 1
+		local right_col = right[3]
+
+		vim.fn.execute("normal! <cr>")
+		local text = vim.api.nvim_buf_get_text(0, left_row, left_col, right_row, right_col, {})
+		vim.pretty_print(text)
+		vim.api.nvim_buf_set_text(0, left_row, left_col, right_row, right_col, { selection[1] })
+		-- vim.fn.execute("normal! vis")
 		-- vim.pretty_print(vim.fn.getpos("'>"))
 		-- vim.pretty_print(vim.fn.getpos({ "some" }))
 		-- vim.api.nvim_put({ selection[1] }, "", false, true)
 	end,
 }
 
+local function trimSentence(sentence)
+	local result = string.gsub(sentence, "%s+", " ")
+	result = string.gsub(result, "%s*%-%-%s*", " ")
+	result = string.gsub(result, "%s*//%s*", " ")
+	result = string.gsub(result, "%s+$", "")
+	result = string.gsub(result, "^%s+", "")
+	vim.pretty_print(result)
+	return result
+end
+
 local function getCurrentSentence()
 	vim.fn.execute('normal! vis"0y')
-	local sentence = vim.fn.getreg("0")
+	local prev = vim.fn.getreg("0")
 
-	sentence = string.gsub(sentence, "%s+", " ")
-	sentence = string.gsub(sentence, "%s*%-%-%s*", " ")
-	sentence = string.gsub(sentence, "%s*$", "")
-	return sentence
+	local new = trimSentence(prev)
+	while new ~= prev do
+		prev = new
+		new = trimSentence(prev)
+	end
+
+	return new
 end
 
 function M.selectSuggestions(opts, items, text)
@@ -89,7 +102,7 @@ local function makeRequest(text)
 		prompt = {
 			original_sentence = text,
 		},
-		temperature = 100,
+		temperature = 0.1,
 		token_count = math.floor(text:len() / 4),
 		n_gen = 3,
 		source_language = "en",
@@ -116,17 +129,13 @@ local function makeRequest(text)
 	end
 
 	for _, obj in ipairs(resp.generated_text) do
-		table.insert(items, obj.text)
+		table.insert(items, trimSentence(obj.text))
 	end
 
 	return items
 end
 
 local sentence = getCurrentSentence()
--- local items = makeRequest(sentence)
-local items = { "some", "suggestions" }
+local items = makeRequest(sentence)
+-- local items = { trimSentence("some very looooo\noooooooooooooooooooooooooooooooooooong item.") }
 M.selectSuggestions(require("telescope.themes").get_dropdown({}), items, sentence)
-
--- this is a test.
---
---
