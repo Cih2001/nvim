3659b6940dbc5b5d49ed19c1a51162f99dd2f21c
Hamidreza Ebtehaj <hamidreza.ebtehaj@gmail.com>
2023-02-27 00:42:48 +0100

    added cmp signature help

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -127,6 +127,7 @@ cmp.setup({
 		end,
 	},
 	sources = {
+		{ name = "nvim_lsp_signature_help" },
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,7 +71,9 @@ return require("packer").startup(function()
 	use("hrsh7th/cmp-path") -- path completions
 	use("hrsh7th/cmp-cmdline") -- cmdline completions
 	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lsp-signature-help")
 	use("saadparwaiz1/cmp_luasnip") -- snippet completions
+
 	-- snippets
 	use("L3MON4D3/LuaSnip") --snippet engine
 	-- debugging
