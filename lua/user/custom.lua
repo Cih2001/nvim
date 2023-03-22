bfaabf4f8d822051a8d9c610b756fadb486cf959
Hamidreza Ebtehaj <hamid.ebtehaj@esgbook.com>
2023-03-22 14:09:34 +0100

    added a command to copy link to the code in github

--- lua/user/custom.lua
+++ lua/user/custom.lua
@@ -1,5 +1,5 @@
 local M = {
-	menu = { "test", "browse pr", "browse pr-env" },
+	menu = { "test", "browse pr", "browse pr-env", "github link" },
 }
 
 local function browsePREnv()
@@ -22,6 +22,17 @@ local function browsePROcto()
 	})
 end
 
+local function getGithubLink()
+	local base = "https://github.com/arabesque-sray/esgbook/blob/main/%s#L%d"
+	local path = vim.api.nvim_eval("expand('%')")
+	local apiIndex = string.find(path, "/api")
+	if apiIndex then
+		path = string.sub(path, apiIndex + 1)
+	end
+	local line = vim.fn.line(".")
+	vim.fn.jobstart({ "open", string.format(base, path, line) })
+end
+
 function M.custom()
 	vim.ui.select(M.menu, { prompt = "select an action" }, function(choice)
 		if choice == "browse pr-env" then
@@ -30,6 +41,8 @@ function M.custom()
 			require("user.gotest").run_current_test()
 		elseif choice == "browse pr" then
 			browsePROcto()
+		elseif choice == "github link" then
+			getGithubLink()
 		end
 	end)
 end
--- lua/user/lsp/settings/gopls.lua
+++ lua/user/lsp/settings/gopls.lua
@@ -7,6 +7,7 @@ return {
 			codelenses = {
 				test = true,
 			},
+			directoryFilters = { "-**/mock_" },
 		},
 	},
 }
