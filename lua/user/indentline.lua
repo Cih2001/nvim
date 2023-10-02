5777f0dcaf4925dd8f2e91d19ad3299e188de50e
Hamidreza Ebtehaj <hamid.ebtehaj@esgbook.com>
2023-10-02 10:51:46 +0200

    updating the blank line

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,20 +1,18 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
 	return
 end
 
-vim.g.indentLine_enabled = 1
--- vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_char = "▏"
-indent_blankline.setup({
-	-- show_end_of_line = true,
-	-- space_char_blankline = " ",
-	-- show_current_context = true,
-	-- show_char_blankline = " "
-	-- show_current_context_start = true,
-	-- char_highlight_list = {
-	--   "IndentBlanklineIndent1",
-	--   "IndentBlanklineIndent2",
-	--   "IndentBlanklineIndent3",
-	-- },
+local highlight = {
+	"IndentBlankLineChar",
+	"IndentBlankLineChar",
+}
+
+ibl.setup({
+	indent = { highlight = highlight, char = "▏" },
+	whitespace = {
+		highlight = highlight,
+		remove_blankline_trail = false,
+	},
+	scope = { enabled = false },
 })
