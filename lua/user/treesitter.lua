123a56f895f15ca120d9a6c0505b17f7cb169b10
Hamidreza Ebtehaj <hamid.ebtehaj@esgbook.com>
2024-11-14 11:32:44 +0100

    clean treesitter autocmd

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -72,25 +72,9 @@ configs.setup({
 	},
 })
 
-local api = vim.api
--- function to create a list of commands and convert them to autocommands
--------- This function is taken from https://github.com/norcalli/nvim_utils
-local function nvim_create_augroups(definitions)
-	for group_name, definition in pairs(definitions) do
-		api.nvim_command("augroup " .. group_name)
-		api.nvim_command("autocmd!")
-		for _, def in ipairs(definition) do
-			local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
-			api.nvim_command(command)
-		end
-		api.nvim_command("augroup END")
-	end
-end
-
-local autoCommands = {
-	open_folds = {
-		{ "BufWinEnter,BufReadPost,FileReadPost", "*", "normal zR" },
-	},
-}
-
-nvim_create_augroups(autoCommands)
+local group_id = vim.api.nvim_create_augroup("open_folds", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufReadPost", "FileReadPost" }, {
+	group = group_id,
+	pattern = { "*.lua", "*.go", "*.yaml", "*.yml" },
+	command = "normal zR",
+})
