a5d1729456790ca711932e356c0dc27ea3c4f428
Hamidreza Ebtehaj <hamid.ebtehaj@esgbook.com>
2025-03-20 15:48:10 +0100

    fix dadbod completion

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -60,6 +60,13 @@ return {
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "buffer" },
+			per_filetype = {
+				sql = { "snippets", "dadbod", "buffer" },
+			},
+			-- add vim-dadbod-completion to your completion providers
+			providers = {
+				dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+			},
 		},
 
 		-- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -1,54 +1,44 @@
-vim.g.db_ui_use_nerd_fonts = 1
-vim.g.db_ui_execute_on_save = 0
-vim.g.db_ui_win_position = "right"
-
-vim.cmd([[
-      autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
-      " Source is automatically added, you just need to include it in the chain complete list
-      let g:completion_chain_complete_list = {
-          \   'sql': [
-          \    {'complete_items': ['vim-dadbod-completion']},
-          \   ],
-          \ }
-      " Make sure `substring` is part of this list. Other items are optional for this completion source
-      let g:completion_matching_strategy_list = ['exact', 'substring']
-      " Useful if there's a lot of camel case items
-      let g:completion_matching_ignore_case = 1
-      ]])
-
-vim.cmd([[
-let g:db_ui_table_helpers = {
-\   'postgresql': {
-\     'List': "select * from {schema}.{table} limit 2;",
-\     'Columns': "",
-\     'Primary Keys': "",
-\     'Indexes': "",
-\     'References': "",
-\     'Foreign Keys': "",
-\     'Info': "select format('%s.%s', table_schema, table_name) as tablename,  column_name, column_default, is_nullable, data_type\n
-\from information_schema.columns where table_name='{table}' and table_schema='{schema}' order by ordinal_position asc;\n
-\select format('%s.%s', schemaname, tablename) as tablename, indexname, indexdef from pg_indexes where tablename='{table}' and schemaname='{schema}';\n
-\select conrelid::regclass as tablename, conname, pg_get_constraintdef(oid) from pg_constraint\n
-\where conrelid::regclass::text = '{schema}.{table}' order by contype desc;\n
-\select conrelid::regclass as dependancies, conname, pg_get_constraintdef(oid) from pg_constraint\n
-\where confrelid::regclass::text = '{schema}.{table}'order by contype desc;"
-\   }
-\ }
-]])
-
-vim.cmd([[
-let g:db_ui_icons = {
-    \ 'expanded': '',
-    \ 'collapsed': '',
-    \ }
-]])
-
-vim.cmd("autocmd FileType sql nmap <buffer> <Leader>e <Plug>(DBUI_EditBindParameters)")
-vim.cmd("autocmd FileType sql nmap <buffer> <Leader>s <Plug>(DBUI_ExecuteQuery)")
-
 return {
 	"kristijanhusak/vim-dadbod-ui",
-	dependencies = { "tpope/vim-dadbod", "kristijanhusak/vim-dadbod-completion" },
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+	},
+	init = function()
+		vim.g.db_ui_use_nerd_fonts = 1
+		vim.g.db_ui_execute_on_save = 0
+		vim.g.db_ui_win_position = "right"
+		vim.g.db_ui_icons = { expanded = "", collapsed = "" }
+		vim.g.vim_dadbod_completion_lowercase_keywords = 1
+		vim.g.db_ui_table_helpers = {
+			postgresql = {
+				List = "select * from {schema}.{table} limit 2;",
+				Columns = "",
+				["Primary Keys"] = "",
+				Indexes = "",
+				References = "",
+				["Foreign Keys"] = "",
+				Info = [[
+select format('%s.%s', table_schema, table_name) as tablename, column_name, column_default, is_nullable, data_type
+from information_schema.columns where table_name='{table}' and table_schema='{schema}' order by ordinal_position asc;
+select format('%s.%s', schemaname, tablename) as tablename, indexname, indexdef from pg_indexes where tablename='{table}' and schemaname='{schema}';
+select conrelid::regclass as tablename, conname, pg_get_constraintdef(oid) from pg_constraint
+where conrelid::regclass::text = '{schema}.{table}' order by contype desc;
+select conrelid::regclass as dependancies, conname, pg_get_constraintdef(oid) from pg_constraint
+where confrelid::regclass::text = '{schema}.{table}' order by contype desc;
+]],
+			},
+		}
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "sql",
+			callback = function()
+				local opts = { noremap = true, silent = true }
+				vim.api.nvim_buf_set_keymap(0, "n", "<Leader>e", "<Plug>(DBUI_EditBindParameters)", opts)
+				vim.api.nvim_buf_set_keymap(0, "n", "<Leader>s", "<Plug>(DBUI_ExecuteQuery)", opts)
+			end,
+		})
+	end,
 	lazy = true,
 	cmd = "DBUI",
 }
