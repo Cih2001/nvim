35f4c8e68d20de7631587ae288aa7f105f6409ad
hamidreza <hamidreza.ebtehaj@gmail.com>
2025-04-02 16:58:28 +0200

    update ai config

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,7 +1,18 @@
 return {
 	"robitx/gp.nvim",
 	config = function()
-		require("gp").setup()
+		require("gp").setup({
+			agents = {
+				{
+					provider = "openai",
+					name = "ChatGPT-o1",
+					chat = true,
+					command = false,
+					model = { model = "o1" },
+					system_prompt = require("gp.defaults").chat_system_prompt,
+				},
+			},
+		})
 	end,
 	lazy = true,
 	keys = {
