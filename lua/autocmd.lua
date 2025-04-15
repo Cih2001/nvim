e8be2f52b8bf76756ba61ba107c1c2ac1266754e
Hamidreza Ebtehaj <hamid.ebtehaj@esgbook.com>
2025-04-15 16:50:16 +0200

    add auto commands

---
+++ lua/autocmd.lua
@@ -0,0 +1,13 @@
+-- spellcheck in md
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	command = "setlocal spell wrap",
+})
+
+-- highlight text on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ timeout = 300, priority = 1000 })
+	end,
+})
