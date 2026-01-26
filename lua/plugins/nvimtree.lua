f050081b6c2e9edadac85ed111eb380d66dfcd6c
Hamidreza Ebtehaj <hamid.ebtehaj@esgbook.com>
2026-01-26 15:10:09 +0100

    prevent nvim-tree resize after opening files

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -18,6 +18,7 @@ return {
 		},
 		view = {
 			width = 30,
+			preserve_window_proportions = true,
 		},
 		renderer = {
 			group_empty = true,
@@ -34,5 +35,34 @@ return {
 		modified = {
 			enable = true,
 		},
+		on_attach = function(bufnr)
+			local api = require("nvim-tree.api")
+
+			-- Default mappings
+			api.config.mappings.default_on_attach(bufnr)
+
+			-- Custom enter key mapping that preserves window width
+			vim.keymap.set("n", "<CR>", function()
+				-- Get the nvim-tree window
+				local tree_winid = vim.api.nvim_get_current_win()
+				local current_width = vim.api.nvim_win_get_width(tree_winid)
+
+				-- Open the file
+				api.node.open.edit()
+
+				-- Schedule the width restoration to happen after the file opens
+				vim.schedule(function()
+					-- Find the nvim-tree window again (in case focus moved)
+					for _, win in ipairs(vim.api.nvim_list_wins()) do
+						local buf = vim.api.nvim_win_get_buf(win)
+						local bufname = vim.api.nvim_buf_get_name(buf)
+						if bufname:match("NvimTree_") then
+							vim.api.nvim_win_set_width(win, current_width)
+							break
+						end
+					end
+				end)
+			end, { buffer = bufnr, noremap = true, silent = true, desc = "Open file and preserve width" })
+		end,
 	},
 }
